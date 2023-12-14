9e9de4ce82a031cf4a3fb0d9ab53bb5350af59f7
TonniPaul <ariyoadeoyepaul@gmail.com>
2023-12-14 15:22:36 +0100

    feat: initial uploas of first svelte app

---
+++ src/lib/card.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+   /**
+	 * @type {string}
+	 */
+      let src : string ;
+     let customClass;
+     let title : string;
+    let description : string;
+
+    export {src, customClass, title, description}
+
+    const cardClass = 'card' + ' ' + customClass
+</script>
+
+<div class={cardClass}>
+   <img {src} alt="car-icon">
+
+   <p class="title-text">{title}</p>
+   <p>{description}</p>
+
+   <button class="card-button">Learn More</button>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+   import Card from '$lib/card.svelte';
+</script>
+
+<style global>
+   @import '../styles/app.styles.css';
+   @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto&display=swap');
+</style>
+
+<main class="main-container">
+   <div class="flex">
+      <Card 
+         src='./icon-sedans.svg'
+         title='sedans'
+         description='Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
+         customClass='' />
+      <Card 
+         src='./icon-suvs.svg'
+         title='suvs'
+         description='Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures,'
+         customClass='secondary' />
+      <Card 
+         src='./icon-luxury.svg'
+         title='luxury'
+         description='Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
+         customClass='tertiary' />
+   </div>
+</main>
